b02b3f001ada0e05999764973b092e44fb991a02
agnibrata98 <agnibratashee@gmail.com>
2024-02-24 15:56:23 +0530

    adding togglebutton

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { motion } from "framer-motion"
 import Links from "./links/Links"
 import "./sidebar.scss"
-import ToggleButton from "./toggleButton/ToggleButton"
+// import ToggleButton from "./toggleButton/ToggleButton"
 function Sidebar() {
     const [open, setOpen] = useState(false)
     const variants = {
@@ -28,7 +28,14 @@ function Sidebar() {
         <motion.div className="bg" variants={variants}>
             <Links/>
         </motion.div>
-        <ToggleButton />
+        <button onClick={()=>setOpen((prev) => !prev)}>
+        <svg width="20" height="20" viewBox="0 0 23 23">
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" d="M 2 9.423 L 20 9.423" variants={{closed:{opacity: 1}, open:{opacity:0},}}/>
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
+        </svg>
+    </button>
+        
     </motion.div>
   )
 }
--- src/components/sidebar/toggleButton/Togglebutton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Togglebutton = () => {
-  return (
-    <div>Togglebutton</div>
-  )
-}
-
-export default Togglebutton
