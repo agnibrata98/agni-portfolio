fe6563a4cf4a4ed82e1c6bd91a56d50383d30d1b
agnibrata98 <agnibratashee@gmail.com>
2024-02-24 15:33:41 +0530

    removing togglebutton

--- src/components/sidebar/toggleButton/Togglebutton.jsx
+++ src/components/sidebar/toggleButton/Togglebutton.jsx
@@ -1,15 +1,14 @@
 import {motion} from "framer-motion"
 
-const ToggleButton = () => {
+const ToggleButton = ({setOpen}) => {
   return (
-    <div></div>
-    // <button onClick={()=>setOpen((prev) => !prev)}>
-    //     <svg width="20" height="20" viewBox="0 0 23 23">
-    //         <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
-    //         <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" d="M 2 9.423 L 20 9.423" variants={{closed:{opacity: 1}, open:{opacity:0},}}/>
-    //         <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
-    //     </svg>
-    // </button> 
+    <button onClick={()=>setOpen((prev) => !prev)}>
+        <svg width="20" height="20" viewBox="0 0 23 23">
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" d="M 2 9.423 L 20 9.423" variants={{closed:{opacity: 1}, open:{opacity:0},}}/>
+            <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" variants={{ closed: {d: "M 2 2.5 L 20 2.5"}, open:{d: "M 3 16.5 L 17 2.5"},}} />
+        </svg>
+    </button> 
   )
 }
 
